4c8a05921616f7cf11359fbbcda5c381258cea23
Coding Saint <codingsaint@gmail.com>
2019-10-21 19:34:19 +0530

    Day3 : Left Rotation and Hourglass

---
+++ src/arrays/leftrotation/Solution.java
@@ -0,0 +1,82 @@
+package arrays.leftrotation;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] array, int d) {
+        int[] rotatedArray= new int[array.length];
+        int k=0;
+        int rotate=0;
+        if(d>array.length) {
+            rotate =d%array.length;
+
+        }else {
+            rotate=d;
+        }
+
+
+        for(int i=rotate;i<array.length;i++) {
+            rotatedArray[k]=array[rotate+k];
+            k++;
+
+        }
+        int l=0;
+        for(int i=k;i<array.length;i++) {
+
+            rotatedArray[i]=array[l];
+            l++;
+
+        }
+
+        return rotatedArray;
+
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
